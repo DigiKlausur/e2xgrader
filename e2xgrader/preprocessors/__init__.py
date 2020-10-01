5ba8ba87d1a163db6501958c50669eaa84960ebb
Tim Metzler <tim.metzler@h-brs.de>
2020-10-01 18:15:31 +0200

    Add preprocessor to filter cells by id

---
+++ e2xgrader/preprocessors/__init__.py
@@ -0,0 +1,1 @@
+from .filtercellsbyid import FilterCellsById
---
+++ e2xgrader/preprocessors/filtercellsbyid.py
@@ -0,0 +1,21 @@
+from nbgrader.preprocessors import NbGraderPreprocessor
+
+class FilterCellsById(NbGraderPreprocessor):
+
+    def __init__(self, **kw):
+        pass
+        
+    def filter_cells(self, cells, keyword):
+        new_cells = []
+        for cell in cells:
+            metadata = cell['metadata']
+            if ('nbgrader' in metadata) and \
+               ('grade_id' in metadata['nbgrader']):
+                if keyword in cell['metadata']['nbgrader']['grade_id']:
+                    new_cells.append(cell)
+        return new_cells
+    
+    def preprocess(self, nb, resources):
+        if 'keyword' in resources:
+            nb.cells = self.filter_cells(nb.cells, resources['keyword'])
+        return nb, resources
