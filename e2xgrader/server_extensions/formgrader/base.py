665888d04e57e00f83657be7fa8ade4219db43eb
Tim Metzler <tim.metzler@h-brs.de>
2020-10-02 17:44:55 +0200

    Use new api for handlers

---
+++ e2xgrader/server_extensions/formgrader/base.py
@@ -0,0 +1,12 @@
+from nbgrader.server_extensions.formgrader.base import BaseApiHandler
+from ...apps.api import E2xAPI
+
+class E2xBaseApiHandler(BaseApiHandler):
+
+    @property
+    def api(self):
+        level = self.log.level
+        api = E2xAPI(
+            self.coursedir, self.authenticator, parent=self.coursedir.parent)
+        api.log_level = level
+        return api    
