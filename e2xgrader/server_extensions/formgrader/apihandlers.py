1e5a4641ad7aa3b8536f27d0ab8d4fcec6f6fa31
Tim Metzler <tim.metzler@h-brs.de>
2020-10-02 17:43:19 +0200

    Add api handlers

---
+++ e2xgrader/server_extensions/formgrader/apihandlers.py
@@ -0,0 +1,27 @@
+import json
+
+from tornado import web
+from nbgrader.server_extensions.formgrader.base import check_xsrf
+
+from .base import E2xBaseApiHandler as BaseApiHandler
+
+class SolutionCellCollectionHandler(BaseApiHandler):
+    @web.authenticated
+    @check_xsrf
+    def get(self, assignment_id, notebook_id):
+        cells = self.api.get_solution_cell_ids(assignment_id, notebook_id)
+        self.write(json.dumps(cells))
+
+
+class SubmittedTaskCollectionHandler(BaseApiHandler):
+    @web.authenticated
+    @check_xsrf
+    def get(self, assignment_id, notebook_id, task_id):
+        submissions = self.api.get_task_submissions(assignment_id, notebook_id, task_id)
+        self.write(json.dumps(submissions))
+
+
+default_handlers = [
+    (r"/formgrader/api/solution_cells/([^/]+)/([^/]+)", SolutionCellCollectionHandler),
+    (r"/formgrader/api/submitted_tasks/([^/]+)/([^/]+)/([^/]+)", SubmittedTaskCollectionHandler),
+]
