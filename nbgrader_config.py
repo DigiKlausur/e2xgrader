3ec91fbefae96a10496c2c244728c3b30ec8132c
Tim Metzler <tim.metzler@h-brs.de>
2020-10-06 19:10:54 +0200

    Add sample nbgrader_config

---
+++ nbgrader_config.py
@@ -0,0 +1,36 @@
+c = get_config()
+
+c.Autograde.sanitize_preprocessors = [
+    'nbgrader.preprocessors.ClearOutput',
+    'nbgrader.preprocessors.DeduplicateIds',
+    'nbgrader.preprocessors.OverwriteKernelspec',
+    'e2xgrader.preprocessors.OverwriteCells',
+    'nbgrader.preprocessors.CheckCellMetadata',
+    'e2xgrader.preprocessors.Unscramble',
+]
+
+c.Autograde.autograde_preprocessors = [
+    'nbgrader.preprocessors.Execute',
+    'nbgrader.preprocessors.LimitOutput',
+    'e2xgrader.preprocessors.SaveAutoGrades',
+    'nbgrader.preprocessors.AssignLatePenalties',
+    'nbgrader.preprocessors.CheckCellMetadata',
+]
+
+c.GenerateAssignment.preprocessors = [
+    'nbgrader.preprocessors.IncludeHeaderFooter',
+    'nbgrader.preprocessors.LockCells',
+    'e2xgrader.preprocessors.ClearSolutions',
+    'nbgrader.preprocessors.ClearOutput',
+    'nbgrader.preprocessors.CheckCellMetadata',
+    'nbgrader.preprocessors.ComputeChecksums',
+    'e2xgrader.preprocessors.SaveCells',
+    'e2xgrader.preprocessors.ClearHiddenTests',
+    'nbgrader.preprocessors.ClearMarkScheme',
+    'nbgrader.preprocessors.ComputeChecksums',
+    'nbgrader.preprocessors.CheckCellMetadata',
+    'e2xgrader.preprocessors.ValidateExtraCells'
+]
+
+c.ExchangeFactory.submit = 'e2xgrader.exchange.E2xExchangeSubmit'
+c.ExchangeFactory.fetch_assignment = 'e2xgrader.exchange.E2xExchangeFetchAssignment'
