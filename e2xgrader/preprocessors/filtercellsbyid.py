6a4dd2c4db14acc88cf3f7326b33f46dd95b4f35
Tim Metzler <tim.metzler@h-brs.de>
2020-10-06 19:44:20 +0200

    Merge if statement

--- e2xgrader/preprocessors/filtercellsbyid.py
+++ e2xgrader/preprocessors/filtercellsbyid.py
@@ -1,17 +1,14 @@
 from nbgrader.preprocessors import NbGraderPreprocessor
 
 class FilterCellsById(NbGraderPreprocessor):
-
-    def __init__(self, **kw):
-        pass
-        
+     
     def filter_cells(self, cells, keyword):
         new_cells = []
         for cell in cells:
             metadata = cell['metadata']
             if ('nbgrader' in metadata) and \
-               ('grade_id' in metadata['nbgrader']):
-                if keyword in cell['metadata']['nbgrader']['grade_id']:
+               ('grade_id' in metadata['nbgrader']) and \
+               (keyword in cell['metadata']['nbgrader']['grade_id']):
                     new_cells.append(cell)
         return new_cells
     
