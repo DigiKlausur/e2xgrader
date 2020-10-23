6ed8e02be5097804a212c591a24597555e781a29
Tim Metzler <tim.metzler@h-brs.de>
2020-10-23 19:11:04 +0200

    Add e2xstudent extensions

---
+++ e2xgrader/nbextensions/student/exam/restricted_tree/main.js
@@ -0,0 +1,25 @@
+define([
+    'require'
+    ], function(
+        require
+    ) {
+
+    "use strict";
+
+    var load_css = function() {
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = require.toUrl("./treeview.css");
+        document.getElementsByTagName("head")[0].appendChild(link);
+    };
+
+    var load_ipython_extension = function () {
+        load_css();
+    }
+
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+
+});
