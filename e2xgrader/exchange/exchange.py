c3a2fcf76142f11a224dee885f9d1c0a1ce3de55
Mohammad Wasil <mwasil@outlook.co.id>
2020-10-28 20:49:42 +0100

    make exchange root readable, and is not writable

--- e2xgrader/exchange/exchange.py
+++ e2xgrader/exchange/exchange.py
@@ -14,3 +14,10 @@ class E2xExchange(Exchange):
         False,
         help='Whether to use a personalized inbound directory per student'
     ).tag(config=True)
+
+    def ensure_root(self):
+         """See if the exchange directory exists and readable, fail if not.
+            We do not need to make the exchange root writable by default.
+         """
+        if not check_directory(self.root, read=True, execute=True):
+            self.fail("Unreadable directory, please contact your instructor: {}".format(self.root))
