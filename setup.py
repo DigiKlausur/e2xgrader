488fddab01a051a0958cd4783d2bec997e46fc7e
Tim Metzler <tim.metzler@h-brs.de>
2021-03-30 16:45:26 +0200

    Drop support for Python 3.5

--- setup.py
+++ setup.py
@@ -36,8 +36,10 @@ setup_args = dict(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     packages=find_packages(),
     package_data={
