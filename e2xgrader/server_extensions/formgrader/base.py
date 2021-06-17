3f712c49c9ee460b1791a7850fff5a765a82a84e
Tim Metzler <tim.metzler@h-brs.de>
2021-06-17 14:35:05 +0200

    Add nbassignment base handlers

--- e2xgrader/server_extensions/formgrader/base.py
+++ e2xgrader/server_extensions/formgrader/base.py
@@ -1,6 +1,9 @@
+import json
+
 from nbgrader.server_extensions.formgrader.base import BaseApiHandler
 from ...apps.api import E2xAPI
-
+from tornado import web
+from nbgrader.server_extensions.formgrader.base import check_xsrf
 
 class E2xBaseApiHandler(BaseApiHandler):
 
@@ -11,3 +14,41 @@ class E2xBaseApiHandler(BaseApiHandler):
             self.coursedir, self.authenticator, parent=self.coursedir.parent)
         api.log_level = level
         return api
+
+
+class BaseApiManageHandler(BaseApiHandler):
+
+    def initialize(self, model):
+        self.__model = model
+
+    @web.authenticated
+    @check_xsrf
+    def post(self, **kwargs):
+        self.write(self.__model.new(**kwargs))
+
+    @web.authenticated
+    @check_xsrf
+    def delete(self, **kwargs):
+        self.__model.remove(**kwargs)
+        self.write({"status": True})
+
+    @web.authenticated
+    @check_xsrf
+    def get(self, **kwargs):
+        self.write(json.dumps(self.__model.get(**kwargs)))
+
+    @web.authenticated
+    @check_xsrf
+    def put(self, **kwargs):        
+        self.write(self.__model.new(**kwargs))
+
+
+class BaseApiListHandler(BaseApiHandler):
+
+    def initialize(self, model):
+        self.__model = model
+
+    @web.authenticated
+    @check_xsrf
+    def get(self, **kwargs):
+        self.write(json.dumps(self.__model.list(**kwargs)))
