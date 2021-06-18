dc9f0c6384a9c7035c12b749a4ac1104a627bb12
Tim Metzler <tim.metzler@h-brs.de>
2021-06-18 17:38:56 +0200

    Add nbassignment converter

---
+++ e2xgrader/converters/__init__.py
@@ -0,0 +1,2 @@
+from .converter import Converter
+from .generateexercise import GenerateExercise
---
+++ e2xgrader/converters/generateexercise.py
@@ -0,0 +1,30 @@
+import tempfile
+from traitlets import List
+from .converter import Converter
+from ..preprocessors.authoring import (
+    RemoveExercise, CopyNotebooks, FillTemplate, CopyFiles,
+    GenerateTaskIDs, AddTaskHeader, MakeExercise)
+
+class GenerateExercise(Converter):
+
+    preprocessors = List([
+        RemoveExercise,
+        CopyNotebooks,
+        FillTemplate,
+        CopyFiles,
+        GenerateTaskIDs,
+        AddTaskHeader,
+        MakeExercise
+    ])
+
+    def __init__(self, coursedir, config=None):
+        super(GenerateExercise, self).__init__(config=config)
+        self.coursedir = coursedir
+
+    def convert(self, resources):
+        with tempfile.TemporaryDirectory() as tmp:
+            resources['tmp_dir'] = tmp
+            resources['course_prefix'] = self.coursedir.root
+            resources['source_dir'] = self.coursedir.source_directory
+            for preprocessor in self._preprocessors:
+                preprocessor.preprocess(resources)
