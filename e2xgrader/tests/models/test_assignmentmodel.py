2f7148182e5aed1d6f356cfe23c11c8db157e54a
Tim Metzler <tim.metzler@h-brs.de>
2021-08-10 00:19:44 +0200

    Add test for assignmentmodel

---
+++ e2xgrader/tests/models/test_assignmentmodel.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+
+from os.path import join as pjoin
+
+from e2xgrader.models import AssignmentModel
+from ..utils.test_utils import createTempCourse
+
+
+
+class TestTemplateModel(unittest.TestCase):
+
+    def setUp(self):
+        tmp_dir, coursedir = createTempCourse()
+        self.tmp_dir = tmp_dir
+        self.model = AssignmentModel(coursedir)
+
+    def test_list_empty(self):
+        assert len(self.model.list()) == 0, 'Model should not list anything in an empty course'
+
+    def tearDown(self):
+        self.tmp_dir.cleanup()
