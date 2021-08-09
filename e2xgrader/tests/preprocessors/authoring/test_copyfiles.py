f12645c3eaa7e27ebb5e18e3da82cb47422908ea
Tim Metzler <tim.metzler@h-brs.de>
2021-08-09 15:05:17 +0200

    Extend test for copyfiles

--- e2xgrader/tests/preprocessors/authoring/test_copyfiles.py
+++ e2xgrader/tests/preprocessors/authoring/test_copyfiles.py
@@ -41,9 +41,10 @@ class TestCopyFiles(BaseTest):
             
     def createTemplate(self):
         self.templatemodel = TemplateModel(self.coursedir)
-        self.templatemodel.new(name=self.resources['template'])
+        self.templatemodel.new(name=self.resources['template'])        
 
-    def createSameFiles(self):
+    def test_copy_identical_files(self):
+        # Create files with the same name and same content for each task
         pool_path = pjoin(self.resources['course_prefix'], 'pools')
         rand_data = str([rd.randint(1, 100000) for _ in range(1000)])
 
@@ -52,8 +53,6 @@ class TestCopyFiles(BaseTest):
             with open(pjoin(data_path, 'randfile'), 'w') as f:
                 f.write(rand_data)
 
-    def test_copy(self):
-        self.createSameFiles()
         res = CopyNotebooks().preprocess(self.resources)
         res = CopyFiles().preprocess(res)
         # Make sure exercise folder is created
@@ -61,5 +60,34 @@ class TestCopyFiles(BaseTest):
                           self.resources['source_dir'],
                           self.resources['assignment'],
                           f"{self.resources['exercise']}_files")
+        data_path = pjoin(file_path, 'data')
         assert os.path.exists(file_path)
-        assert os.path.exists(pjoin(file_path, 'data', 'randfile'))
+        assert os.path.exists(data_path)
+        data_files = os.listdir(data_path)
+        assert len(data_files) == 1
+        assert data_files[0] == 'randfile'
+
+    def test_copy_different_files(self):
+        # Create files with the same name but different content for each task
+        pool_path = pjoin(self.resources['course_prefix'], 'pools')
+
+        for task_dict in self.resources['tasks']:
+            data_path = pjoin(pool_path, task_dict['pool'], task_dict['task'], 'data')
+            with open(pjoin(data_path, 'randfile'), 'w') as f:
+                f.write(str([rd.randint(1, 100000) for _ in range(1000)]))
+
+        res = CopyNotebooks().preprocess(self.resources)
+        res = CopyFiles().preprocess(res)
+        # Make sure exercise folder is created
+        file_path = pjoin(self.resources['course_prefix'], 
+                          self.resources['source_dir'],
+                          self.resources['assignment'],
+                          f"{self.resources['exercise']}_files")
+        data_path = pjoin(file_path, 'data')
+        assert os.path.exists(file_path)
+        assert os.path.exists(data_path)
+        data_files = os.listdir(data_path)
+        assert len(data_files) == len(self.resources['tasks'])
+        assert 'randfile' in data_files
+        for i in range(len(self.resources['tasks']) - 1):
+            assert f'randfile_{i+1}' in data_files
