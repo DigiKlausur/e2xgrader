992461ef3ab729082fd7e4bff2ec02dbd5bd083f
Tim Metzler <tim.metzler@h-brs.de>
2021-08-10 14:41:03 +0200

    Add tests to notebookvariableextractor test

--- e2xgrader/tests/utils/test_notebookvariableextractor.py
+++ e2xgrader/tests/utils/test_notebookvariableextractor.py
@@ -1,8 +1,10 @@
 import unittest
 
+from e2xgrader.models import TemplateModel
 from e2xgrader.utils import NotebookVariableExtractor
 
-from ..test_utils.test_utils import createTempCourse, add_template_with_header
+from ..test_utils.test_utils import (createTempCourse, add_template_with_header, 
+                                     add_header_to_template, add_footer_to_template)
 
 
 class TestNotebookVariableExtractor(unittest.TestCase):
@@ -12,7 +14,7 @@ class TestNotebookVariableExtractor(unittest.TestCase):
         self.tmp_dir = tmp_dir
         self.coursedir = coursedir
 
-    def test_extract_variables(self):
+    def test_extract_variables_single_cell(self):
         path = add_template_with_header(
             self.coursedir, 
             'TestTemplate',
@@ -22,6 +24,19 @@ class TestNotebookVariableExtractor(unittest.TestCase):
 
         assert len(variables) == 3
         for variable in ['variable1', 'variable_2', 'variable_3']:
+            assert variable in variables, f'{variable} not found'
+
+    def test_extract_variables_multiple_cells(self):
+        res = TemplateModel(self.coursedir).new(name='TestTemplate')
+        path = res['path']
+
+        add_header_to_template(self.coursedir, path, '{{ header_variable_1 }}\n{{header_variable_2}}')
+        add_footer_to_template(self.coursedir, path, '{{ footer_variable_1 }}\n{{footer_variable_2}}')
+
+        variables = NotebookVariableExtractor().extract(path)
+
+        assert len(variables) == 4
+        for variable in ['header_variable_1', 'header_variable_2', 'footer_variable_1', 'footer_variable_2']:
             assert variable in variables, f'{variable} not found'
 
     def test_extract_no_variables(self):
