2431af62789cc904a25f985fa8f6ccb3d00d5334
Tim Metzler <tim.metzler@h-brs.de>
2021-10-05 16:38:09 +0200

    Add modular graders

---
+++ e2xgrader/graders/code.py
@@ -0,0 +1,61 @@
+from .base import BaseGrader
+from nbformat.notebooknode import NotebookNode
+from logging import Logger
+from typing import Optional, Tuple
+from nbgrader.utils import is_solution, get_partial_grade
+
+
+class CodeGrader(BaseGrader):
+    def extract_grade(self, text, log: Logger = None):
+        delimiter_start = "### BEGIN GRADE"
+        delimiter_end = "### END GRADE"
+        inside = False
+
+        points = None
+
+        grade_lines = []
+
+        for line in text.split("\n"):
+            if line.startswith(delimiter_start):
+                inside = True
+            elif line.startswith(delimiter_end):
+                inside = False
+                break
+            elif inside:
+                grade_lines.append(line)
+
+        if len(grade_lines) > 0 and not inside:
+            # Try casting result to float
+            try:
+                points = float("".join(grade_lines))
+            except ValueError as ve:
+                pass
+        return points
+
+    def determine_grade(
+        self, cell: NotebookNode, log: Logger = None
+    ) -> Tuple[Optional[float], float]:
+        max_points = float(cell.metadata["nbgrader"]["points"])
+
+        if not self.cell_changed(cell):
+            return 0, max_points
+        elif is_solution(cell):
+            return None, max_points
+
+        for output in cell.outputs:
+            # option 1: error, return 0
+            if (
+                output.output_type == "error"
+                or output.output_type == "stream"
+                and output.name == "stderr"
+            ):
+                return 0, max_points
+            if output.output_type == "execute_result":
+                partial_grade = get_partial_grade(output, max_points, log)
+                return partial_grade, max_points
+            if output.output_type == "stream" and output.name == "stdout":
+                partial_grade = self.extract_grade(output.text, log)
+                if partial_grade is not None:
+                    return partial_grade, max_points
+
+        return max_points, max_points
