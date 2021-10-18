6be66801ecf42c0ef2a5dc89a4a2b26c1c49be75
Mohammad Wasil <mwasil@outlook.co.id>
2021-10-18 16:01:03 +0200

    [exchange/collect] added missing configs from collect api

--- e2xgrader/exchange/collect.py
+++ e2xgrader/exchange/collect.py
@@ -10,6 +10,21 @@ from nbgrader.api import Gradebook, MissingEntry
 
 
 class E2xExchangeCollect(E2xExchange, ExchangeCollect):
+
+    update = Bool(
+        False, help="Update existing submissions with ones that have newer timestamps."
+    ).tag(config=True)
+
+    before_duedate = Bool(
+        False,
+        help="Collect the last submission before due date or the last submission if no submission before due date.",
+    ).tag(config=True)
+
+    check_owner = Bool(
+        default_value=True,
+        help="Whether to cross-check the student_id with the UNIX-owner of the submitted directory.",
+    ).tag(config=True)
+
     def init_submissions(self):
         if self.personalized_inbound:
             self.log.info("Collecting from restricted submit dirs")
