54d0cdde0285e403f4b2e78888f439e0551b9e2a
Tim Metzler <tim.metzler@h-brs.de>
2022-02-24 15:10:20 +0100

    Add grader for form cells

--- e2xgrader/graders/__init__.py
+++ e2xgrader/graders/__init__.py
@@ -2,5 +2,6 @@ from .base import BaseGrader
 from .code import CodeGrader
 from .multiplechoice import MultipleChoiceGrader
 from .singlechoice import SingleChoiceGrader
+from .form import FormCellGrader
 
-__all__ = ["BaseGrader", "CodeGrader", "MultipleChoiceGrader", "SingleChoiceGrader"]
+__all__ = ["BaseGrader", "CodeGrader", "MultipleChoiceGrader", "SingleChoiceGrader", "FormCellGrader"]
