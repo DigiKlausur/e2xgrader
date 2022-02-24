49f4176fdf6cf5a7a7128e876f2d1a6d5e96e216
Tim Metzler <tim.metzler@h-brs.de>
2022-02-24 16:57:10 +0100

    Add test stub to FilterTests

--- e2xgrader/preprocessors/filtertests.py
+++ e2xgrader/preprocessors/filtertests.py
@@ -1,7 +1,7 @@
 from nbgrader.preprocessors import NbGraderPreprocessor
 from nbformat.notebooknode import NotebookNode
 from nbconvert.exporters.exporter import ResourcesDict
-from traitlets import Bool
+from traitlets import Bool, Unicode
 from typing import Tuple
 from nbgrader.utils import is_grade, is_solution
 
@@ -12,10 +12,14 @@ class FilterTests(NbGraderPreprocessor):
         False, help="Hide test cells in the feedback generated for students."
     ).tag(config=True)
 
+    test_stub = Unicode(
+        "# This test case is hidden #",
+        help="Replace test cell source with this message",
+    ).tag(config=True)
+
     def preprocess_cell(
         self, cell: NotebookNode, resources: ResourcesDict, cell_index: int
     ) -> Tuple[NotebookNode, ResourcesDict]:
-        if self.hide_cells:
-            if not is_solution(cell) and is_grade(cell):
-                cell.source = "# This test case is hidden #"
+        if self.hide_cells and not is_solution(cell) and is_grade(cell):
+            cell.source = "# This test case is hidden #"
         return cell, resources
