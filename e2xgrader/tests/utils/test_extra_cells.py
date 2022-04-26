37d195fd3e2ab85560a43ab0c67074ccc6b99d4a
Tim Metzler <tim.metzler@h-brs.de>
2022-04-26 15:39:25 +0200

    Add tests for diagram and attachment cell

--- e2xgrader/tests/utils/test_extra_cells.py
+++ e2xgrader/tests/utils/test_extra_cells.py
@@ -6,6 +6,8 @@ from e2xgrader.utils.extra_cells import (
     is_extra_cell,
     is_multiplechoice,
     is_singlechoice,
+    is_diagram,
+    is_attachment_cell,
     get_choices,
     get_num_of_choices,
     clear_choices,
@@ -20,6 +22,9 @@ class TestExtraCells(unittest.TestCase):
         self.tmp_dir = tmp_dir
         self.model = PresetModel(coursedir)
         self.multiplechoice = "Multiple Choice"
+        self.singlechoice = "Single Choice"
+        self.diagram = "Diagram"
+        self.attachment = "Upload Files"
 
     def test_extra_cell(self):
         assert not is_extra_cell(nbformat.v4.new_code_cell())
@@ -32,7 +37,7 @@ class TestExtraCells(unittest.TestCase):
         assert is_multiplechoice(cells[0])
 
     def test_singlechoice_cell(self):
-        cells = self.model.get_question_preset("Single Choice")
+        cells = self.model.get_question_preset(self.singlechoice)
         assert is_singlechoice(cells[0])
 
     def test_get_choices(self):
@@ -44,8 +49,11 @@ class TestExtraCells(unittest.TestCase):
         assert len(get_choices(cells[0])) == 3
         assert all([i in get_choices(cells[0]) for i in [1, 2, 3]])
 
+        cells = self.model.get_question_preset(self.diagram)
+        assert len(get_choices(cells[0])) == 0
+
     def test_get_num_of_choices(self):
-        cells = self.model.get_question_preset("Single Choice")
+        cells = self.model.get_question_preset(self.singlechoice)
         assert get_num_of_choices(cells[0]) is None
 
         cells = self.model.get_question_preset(self.multiplechoice)
@@ -61,6 +69,17 @@ class TestExtraCells(unittest.TestCase):
     def test_has_solution(self):
         cells = self.model.get_question_preset(self.multiplechoice)
         assert not has_solution(cells[0])
+
+        cells = self.model.get_question_preset(self.diagram)
+        assert not has_solution(cells[0])
+
+    def test_diagram_cell(self):
+        cells = self.model.get_question_preset(self.diagram)
+        assert is_diagram(cells[1])
+
+    def test_attachment_cell(self):
+        cells = self.model.get_question_preset(self.attachment)
+        assert is_attachment_cell(cells[1])
 
     def tearDown(self):
         self.tmp_dir.cleanup()
