df2d64660bb916275e0e3d3414eaec6fdd59197b
Tim Metzler <tim.metzler@h-brs.de>
2022-05-03 17:43:31 +0200

    Refactor unscramble

--- e2xgrader/preprocessors/unscramble.py
+++ e2xgrader/preprocessors/unscramble.py
@@ -10,18 +10,16 @@ class Unscramble(NbGraderPreprocessor):
         self.log.info("Init Unscramble")
 
     def preprocess(self, nb, resources):
-        if "scramble_config" not in nb.metadata:
-            return nb, resources
+        if "scramble_config" in nb.metadata:
+            byte_config = nb.metadata.scramble_config.config
+            config = pickle.loads(base64.b85decode(byte_config))
 
-        byte_config = nb.metadata.scramble_config.config
-        config = pickle.loads(base64.b85decode(byte_config))
-
-        for cell in nb.cells:
-            matches = self.__pattern.findall(cell.source)
-            for m in matches:
-                if m.strip() in config:
-                    cell.source = cell.source.replace(
-                        "{{" + m + "}}", str(config[m.strip()])
-                    )
+            for cell in nb.cells:
+                matches = self.__pattern.findall(cell.source)
+                for m in matches:
+                    if m.strip() in config:
+                        cell.source = cell.source.replace(
+                            "{{" + m + "}}", str(config[m.strip()])
+                        )
 
         return nb, resources
