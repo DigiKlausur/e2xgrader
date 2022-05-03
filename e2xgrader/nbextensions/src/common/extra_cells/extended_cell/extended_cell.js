794734127630395afc76ac7cd912b8fffd05d28d
Tim Metzler <tim.metzler@h-brs.de>
2022-05-03 15:33:34 +0200

    Use option dict in extended cell

--- e2xgrader/nbextensions/src/common/extra_cells/extended_cell/extended_cell.js
+++ e2xgrader/nbextensions/src/common/extra_cells/extended_cell/extended_cell.js
@@ -1,7 +1,9 @@
 define([
-    'jquery'
+    'jquery',
+    './options'
 ], function(
-    $
+    $,
+    option_dict,
 ) {
 
     'use strict';
@@ -17,28 +19,8 @@ define([
                 this.cell.metadata[this.field] = {};
             }
             this.cell.metadata[this.field]['type'] = type;
-            this.update_options(options);
+            this.option_dict = new option_dict.OptionDict(cell, options);
             this.edit_mode = false;
-        }
-
-        update_options(options) {
-            // Remove all keys that are not in the options dict
-            // Add all keys that are not in metadata
-            if (!this.cell.metadata[this.field].hasOwnProperty('options')) {
-                this.cell.metadata[this.field]['options'] = options;
-                return
-            }
-
-            let old_options = this.cell.metadata[this.field]['options'];
-            let updated_options = {};
-            for (const [key, value] of Object.entries(options)) {
-                if (old_options.hasOwnProperty(key)) {
-                    updated_options[key] = old_options[key];
-                } else {
-                    updated_options[key] = value;
-                }
-            }
-            this.cell.metadata[this.field]['options'] = updated_options;
         }
 
         get_metadata() {
@@ -60,7 +42,7 @@ define([
 
             container.append($('<hr/>'));
 
-            container.append(this.get_options());
+            container.append(this.option_dict.render());
             container.append($('<button/>')
                 .attr('type', 'button')
                 .addClass('e2x_unrender')
@@ -68,30 +50,6 @@ define([
                     that.cell.unrender_force();
                 }).append('Edit cell'));
             html.append(container);
-        }
-
-        get_options() {
-            let that = this;
-            let container = $('<div/>').addClass('e2x_options');
-
-            for (const [key, value] of Object.entries(this.cell.metadata[this.field]['options'])) {
-
-                if (value['type'] == 'checkbox') {
-                    let node = $('<div/>');
-                    let input = $('<input/>').attr('type', value['type']);
-                    node.append(input);
-                    node.append($('<span/>').text(value['text']));                    
-
-                    if (value['value']) {
-                        input.attr('checked', 'checked');
-                    }
-                    input.change(function() {
-                        that.cell.metadata[that.field]['options'][key]['value'] = !!this.checked;
-                    });
-                    container.append(node);
-                }
-            }
-            return container;
         }
 
         render() {
