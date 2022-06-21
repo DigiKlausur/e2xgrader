afe3d6f45bff87f3eccaaa855eab0559f34d2273
Tim Metzler <tim.metzler@h-brs.de>
2022-06-21 18:26:03 +0200

    Remove dependencies from setup.py

--- setup.py
+++ setup.py
@@ -56,16 +56,9 @@ setup_args = dict(
     },
     entry_points={"console_scripts": ["e2xgrader=e2xgrader.apps.e2xgraderapp:main"]},
     install_requires=[
-        "jupyter",
-        "notebook>=6.1.6",
-        "nbformat",
-        "jupyter_core",
-        "jupyter_client",
-        "tornado",
-        "requests",
+        "nbgrader @ git+https://github.com/jupyter/nbgrader@0.7.x",
         "beautifulsoup4",
         "pandas",
-        "nbgrader @ git+https://github.com/jupyter/nbgrader@0.7.x",
     ],
     cmdclass={
         "build_py": js_prerelease(build_py),
