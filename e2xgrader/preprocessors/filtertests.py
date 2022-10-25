ff3f65c9cd4b533a7760a1b45259d684aa7e7359
Tim Daniel Metzler <tim.metzler@h-brs.de>
2022-10-25 17:21:44 +0200

    Add custom message to filtertests

--- e2xgrader/preprocessors/filtertests.py
+++ e2xgrader/preprocessors/filtertests.py
@@ -4,7 +4,7 @@ from nbconvert.exporters.exporter import ResourcesDict
 from nbformat.notebooknode import NotebookNode
 from nbgrader.preprocessors import NbGraderPreprocessor
 from nbgrader.utils import is_grade, is_solution
-from traitlets import Bool
+from traitlets import Bool, Unicode
 
 
 class FilterTests(NbGraderPreprocessor):
@@ -13,9 +13,14 @@ class FilterTests(NbGraderPreprocessor):
         False, help="Hide test cells in the feedback generated for students."
     ).tag(config=True)
 
+    test_case_stub = Unicode(
+        "# This test case is hidden #",
+        help="The message the test case source is replaced with",
+    ).tag(config=True)
+
     def preprocess_cell(
         self, cell: NotebookNode, resources: ResourcesDict, cell_index: int
     ) -> Tuple[NotebookNode, ResourcesDict]:
         if self.hide_cells and is_grade(cell) and not is_solution(cell):
-            cell.source = "# This test case is hidden #"
+            cell.source = self.test_case_stub
         return cell, resources
