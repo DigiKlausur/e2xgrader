ae3becac275b433b8e0ba1dbf974344c3116a406
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-03-29 16:04:01 +0200

    Add a data table component

---
+++ packages/application/src/components/DataTable.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+
+export default function DataTable(rows, ...props) {
+  const [pageSize, setPageSize] = React.useState(20);
+
+  return (
+    <DataGrid
+      {...props}
+      rows={rows || []}
+      density="compact"
+      autoHeight
+      pagination
+      pageSize={pageSize}
+      rowsPerPageOptions={[5, 10, 20, 50, 100]}
+      onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+      disableSelectionOnClick
+      disableColumnFilter
+      disableColumnSelector
+      disableDensitySelector
+      components={{ Toolbar: GridToolbar }}
+      componentsProps={{
+        toolbar: {
+          showQuickFilter: true,
+          quickFilterProps: { debounceMs: 500 },
+        },
+      }}
+    />
+  );
+}
