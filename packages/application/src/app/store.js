d69376651c5bbc27490b9a08ac1d3a53057c8b5e
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-03-29 16:06:00 +0200

    Add a redux store

---
+++ packages/application/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "../api";
+
+export const store = configureStore({
+    reducer: {
+      [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+  });
+  
+setupListeners(store.dispatch);
