58fab09470e8d8794b3a8e780d083e5204f43cc3
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-03-29 16:05:47 +0200

    Create a base api

---
+++ packages/application/src/api/base.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+import { getCookie, urlJoin, ROOT } from "./utils";
+
+const NBGRADER_API_ROOT = urlJoin(ROOT, "formgrader", "api");
+
+const baseSettings = {
+  credentials: "same-origin",
+  headers: {
+    "X-CSRFToken": getCookie("_xsrf"),
+  },
+};
+  
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: NBGRADER_API_ROOT, ...baseSettings}),
+  endpoints: () => ({}),
+});
