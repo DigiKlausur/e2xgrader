044285303f316e4ed1173c7a7c9505fb6403c9fb
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-03-30 17:29:33 +0200

    Use add student dialog

--- packages/application/src/features/students/Students.jsx
+++ packages/application/src/features/students/Students.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Breadcrumbs, Typography } from "@mui/material";
+import { Breadcrumbs, Stack, Typography } from "@mui/material";
 
 import StudentsTable from "./StudentsTable";
+import AddStudentDialog from "./AddStudentDialog";
 
 export default function Students() {
   return (
-    <>
+    <Stack spacing={2}>
       <Breadcrumbs separator=">">
         <Typography color="text.primary">Students</Typography>
       </Breadcrumbs>
       <StudentsTable />
-    </>
+      <Stack direction="row">
+        <AddStudentDialog />
+      </Stack>
+    </Stack>
   );
 }
