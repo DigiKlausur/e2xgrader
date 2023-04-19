2b967db6e56d4118be050e27e6ac7e6bb41d732d
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-04-19 15:47:00 +0200

    Use shortcut manager from utils

--- packages/restricted-exam-notebook/src/disable-shortcuts.js
+++ packages/restricted-exam-notebook/src/disable-shortcuts.js
@@ -1,4 +1,4 @@
-import Jupyter from "base/js/namespace";
+import { shortcuts as utils } from "@e2xgrader/utils";
 
 export function disable_shortcuts() {
   const shortcuts = [
@@ -12,72 +12,24 @@ export function disable_shortcuts() {
     "y",
     "m",
     "r",
-    "shift-enter",
-    "alt-enter",
     "ctrl-shift-f",
     "ctrl-shift-p",
     "p",
     "d,d",
   ];
 
-  for (let i in shortcuts) {
-    try {
-      Jupyter.keyboard_manager.command_shortcuts.remove_shortcut(shortcuts[i]);
-      Jupyter.keyboard_manager.edit_shortcuts.remove_shortcut(shortcuts[i]);
-    } catch (e) {
-      // Shortcut does not exist
-    }
-  }
+  utils.remove_shortcuts("command", shortcuts);
+  utils.remove_shortcuts("edit", shortcuts);
 
-  try {
-    Jupyter.keyboard_manager.command_shortcuts.remove_shortcut("ctrl-v");
-  } catch (e) {
-    console.log("Command shortcut " + "ctrl-v" + " does not exist.");
-  }
+  utils.remove_shortcuts("command", "crtl-v");
 
-  // Bind all execute cell shortcuts to run cell
-
-  Jupyter.keyboard_manager.command_shortcuts.add_shortcut("alt-enter", {
-    help: "run cell",
-    help_index: "zz",
-    handler: function (event) {
-      IPython.notebook.execute_cell();
+  utils.disable_add_cell_on_execute();
+  utils.add_shortcut(
+    "command",
+    "ctrl-v",
+    function (event) {
       return false;
     },
-  });
-
-  Jupyter.keyboard_manager.command_shortcuts.add_shortcut("shift-enter", {
-    help: "run cell",
-    help_index: "zz",
-    handler: function (event) {
-      IPython.notebook.execute_cell();
-      return false;
-    },
-  });
-
-  Jupyter.keyboard_manager.edit_shortcuts.add_shortcut("alt-enter", {
-    help: "run cell",
-    help_index: "zz",
-    handler: function (event) {
-      IPython.notebook.execute_cell();
-      return false;
-    },
-  });
-
-  Jupyter.keyboard_manager.edit_shortcuts.add_shortcut("shift-enter", {
-    help: "run cell",
-    help_index: "zz",
-    handler: function (event) {
-      IPython.notebook.execute_cell();
-      return false;
-    },
-  });
-
-  Jupyter.keyboard_manager.command_shortcuts.add_shortcut("ctrl-v", {
-    help: "no action",
-    help_index: "zz",
-    handler: function (event) {
-      return false;
-    },
-  });
+    "no action"
+  );
 }
