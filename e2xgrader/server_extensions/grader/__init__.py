b523ae2c9b79ad9e5b4cc9fdddbfe28fca0ac0a5
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-05-15 16:15:41 +0200

    Make exports explicit

--- e2xgrader/server_extensions/grader/__init__.py
+++ e2xgrader/server_extensions/grader/__init__.py
@@ -1,1 +1,5 @@
 from .grader import load_jupyter_server_extension
+
+__all__ = [
+    "load_jupyter_server_extension",
+]
--- e2xgrader/server_extensions/grader/apps/e2xgraderapi/__init__.py
+++ e2xgrader/server_extensions/grader/apps/e2xgraderapi/__init__.py
@@ -1,1 +1,5 @@
 from .e2xgraderapi import E2xGraderApi
+
+__all__ = [
+    "E2xGraderApi",
+]
