494a0ebb1f8aef48e7f147397276d5675b406c8c
Tim Metzler <tim.metzler@h-brs.de>
2023-05-20 01:25:31 +0200

    Add student server extension

---
+++ e2xgrader/server_extensions/student/student.py
@@ -0,0 +1,18 @@
+from traitlets import Any, List
+
+from ..apps.assignment_list import AssignmentList
+from ..apps.validate_assignment import ValidateAssignment
+from ..base import BaseExtension
+
+
+class StudentExtension(BaseExtension):
+    apps = List(
+        trait=Any(),
+        default_value=[AssignmentList, ValidateAssignment],
+    ).tag(config=True)
+
+
+def load_jupyter_server_extension(nbapp):
+    """Load the e2xgrader serverextension"""
+    nbapp.log.info("Loading the e2xgrader student serverextension")
+    StudentExtension(parent=nbapp)
