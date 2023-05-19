3917edae0d92cfea14dd4781ceb0e1a73c922485
Tim Metzler <tim.metzler@h-brs.de>
2023-05-20 01:25:17 +0200

    Add teacher server extension

---
+++ e2xgrader/server_extensions/teacher/teacher.py
@@ -0,0 +1,29 @@
+from traitlets import Any, List
+
+from ..apps.assignment_list import AssignmentList
+from ..apps.authoring import AuthoringApp
+from ..apps.e2xgraderapi import E2xGraderApi
+from ..apps.formgrader import FormgradeApp
+from ..apps.nbgraderapi import NbGraderApi
+from ..apps.validate_assignment import ValidateAssignment
+from ..base import BaseExtension
+
+
+class TeacherExtension(BaseExtension):
+    apps = List(
+        trait=Any(),
+        default_value=[
+            E2xGraderApi,
+            NbGraderApi,
+            FormgradeApp,
+            AuthoringApp,
+            ValidateAssignment,
+            AssignmentList,
+        ],
+    ).tag(config=True)
+
+
+def load_jupyter_server_extension(nbapp):
+    """Load the e2xgrader serverextension"""
+    nbapp.log.info("Loading the e2xgrader teacher serverextension")
+    TeacherExtension(parent=nbapp)
