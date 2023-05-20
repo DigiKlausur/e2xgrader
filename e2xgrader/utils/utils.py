464e3243724b649939806d4fcff8b871a63ec691
Tim Metzler <tim.metzler@h-brs.de>
2023-05-20 23:47:40 +0200

    Move utils to e2xgrader_base_app

--- e2xgrader/utils/utils.py
+++ e2xgrader/utils/utils.py
@@ -1,19 +1,4 @@
-import re
-from typing import List
-from urllib.parse import parse_qsl, urlencode, urlparse
-
 from nbgrader.apps import NbGrader
-
-
-def urljoin(*parts: List[str]) -> str:
-    return re.sub(r"/+", r"/", ("/" + "/".join(parts)))
-
-
-def format_url(url, params):
-    parsed = urlparse(url)
-    return parsed._replace(
-        query=urlencode({**dict(parse_qsl(parsed.query)), **params})
-    ).geturl()
 
 
 def get_nbgrader_config():
