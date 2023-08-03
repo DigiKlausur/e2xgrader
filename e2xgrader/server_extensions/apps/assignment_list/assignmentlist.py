c957cd2bca57204329bbc7478e6aa9cece7738b5
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-08-03 16:54:32 +0200

    Ensure compatibility between notebook and new server

--- e2xgrader/server_extensions/apps/assignment_list/assignmentlist.py
+++ e2xgrader/server_extensions/apps/assignment_list/assignmentlist.py
@@ -5,4 +5,15 @@ from .handlers import default_handlers
 
 class AssignmentList(BaseApp):
     def load_app(self):
+        self.log.info("Loading the assignment list app")
+        self.log.info(self.parent.notebook_dir)
+        if self.parent.name == "jupyter-notebook":
+            self.update_tornado_settings(
+                dict(assignment_list_manager=self.parent.notebook_dir)
+            )
+        else:
+            self.update_tornado_settings(
+                dict(assignment_list_manager=self.parent.root_dir)
+            )
+
         self.add_handlers(default_handlers)
