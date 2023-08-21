52f3a2e697bc4719f76968ed75e62b8e30cdee5e
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-08-21 12:55:02 +0200

    Add log message to validate assignment app

--- e2xgrader/server_extensions/apps/validate_assignment/validateassignment.py
+++ e2xgrader/server_extensions/apps/validate_assignment/validateassignment.py
@@ -5,4 +5,5 @@ from .handlers import default_handlers
 
 class ValidateAssignment(BaseApp):
     def load_app(self):
+        self.log.info("Loading the e2x validate assignment app")
         self.add_handlers(default_handlers)
