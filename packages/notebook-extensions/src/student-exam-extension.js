3a4846aaad199e983ced8a55fcf902fb57be04b5
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-08-25 17:55:31 +0200

    Use optional chain expression for readability

--- packages/notebook-extensions/src/student-exam-extension.js
+++ packages/notebook-extensions/src/student-exam-extension.js
@@ -15,7 +15,7 @@ function initialize() {
 }
 
 export function load_ipython_extension() {
-  if (Jupyter.notebook !== undefined && Jupyter.notebook._fully_loaded) {
+  if (Jupyter.notebook?._fully_loaded) {
     initialize();
   } else {
     events.on("notebook_loaded.Notebook", initialize);
