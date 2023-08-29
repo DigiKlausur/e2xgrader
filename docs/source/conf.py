7ab796a5f27758765d7b354a9e5f97ded7ef4339
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-08-29 18:15:48 +0200

    Bump to 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "e2xgrader"
 copyright = "2023, Tim Metzler"
 author = "Tim Metzler"
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
