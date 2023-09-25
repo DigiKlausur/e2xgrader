9563f2b3d82531f60d217897289b833d7a682de7
Tim Metzler <tim.metzler@inf.h-brs.de>
2023-09-25 15:47:24 +0200

    Bump to 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "e2xgrader"
 copyright = "2023, Tim Metzler"
 author = "Tim Metzler"
-release = "0.2.0-dev1"
+release = "0.2.0"
 github_project_url = "https://github.com/Digiklausur/e2xgrader"
 
 # -- General configuration ---------------------------------------------------
