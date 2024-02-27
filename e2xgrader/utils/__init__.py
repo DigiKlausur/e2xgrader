ee64313c6070c198974a7ced2de0e1b3907b1cb8
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-02-27 19:15:33 +0100

    Remove get_nbgrader_config function

--- e2xgrader/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import get_nbgrader_config
-
-__all__ = ["get_nbgrader_config"]
--- e2xgrader/utils/utils.py
+++
@@ -1,7 +0,0 @@
-from nbgrader.apps import NbGrader
-
-
-def get_nbgrader_config():
-    nbgrader = NbGrader()
-    nbgrader.initialize([])
-    return nbgrader.config
