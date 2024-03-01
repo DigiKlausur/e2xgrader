11af8fe7825488d759b7aba3cfaae3200dcd1e3e
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-03-01 13:57:41 +0100

    Use e2xgrader mode enum in baseapp

--- e2xgrader/apps/baseapp.py
+++ e2xgrader/apps/baseapp.py
@@ -1,15 +1,22 @@
+from textwrap import dedent
+
 from jupyter_core.application import JupyterApp
 from traitlets import Enum
 
-from ..utils.mode import infer_e2xgrader_mode
+from ..utils.mode import E2xGraderMode, infer_e2xgrader_mode
 
 
 class E2xGrader(JupyterApp):
 
     mode = Enum(
-        values=["teacher", "student", "student_exam", "None"],
-        default_value="None",
-        help="Which mode is activated, can be teacher, student or student_exam",
+        values=[mode.value for mode in E2xGraderMode],
+        default_value=E2xGraderMode.INACTIVE.value,
+        help=dedent(
+            """
+            Which mode is activated, can be teacher, student, student_exam or deactivated.
+            Is set to invalid if the mode cannot be inferred.
+        """
+        ),
     )
 
     def fail(self, msg, *args):
@@ -22,4 +29,5 @@ class E2xGrader(JupyterApp):
             self.mode = mode
         except ValueError as e:
             self.log.error(str(e))
+            self.mode = E2xGraderMode.INVALID.value
         super().initialize(argv)
