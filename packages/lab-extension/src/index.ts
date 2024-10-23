95a7f50824b3cf21a6d2f0cc8c8297ae211d22c2
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-10-23 15:00:51 +0200

    Add new hello world lab-extension

---
+++ packages/lab-extension/src/index.ts
@@ -0,0 +1,41 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+/**
+ * Initialization data for the @e2xgrader/labextension extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@e2xgrader/labextension:plugin',
+  description: 'A JupyterLab extension which bundles e2xgrader extensions',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (
+    app: JupyterFrontEnd,
+    settingRegistry: ISettingRegistry | null
+  ) => {
+    console.log('JupyterLab extension @e2xgrader/labextension is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log(
+            '@e2xgrader/labextension settings loaded:',
+            settings.composite
+          );
+        })
+        .catch(reason => {
+          console.error(
+            'Failed to load settings for @e2xgrader/labextension.',
+            reason
+          );
+        });
+    }
+  }
+};
+
+export default [plugin];
