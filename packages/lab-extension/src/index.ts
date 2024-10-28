3978fb8f4a72e58a6572e2b678aa289a4e27367a
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-10-28 18:12:34 +0100

    Update lab-extension

--- packages/lab-extension/src/index.ts
+++ packages/lab-extension/src/index.ts
@@ -3,7 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { ICellRegistry } from '@e2xgrader/cell-core';
 
 /**
  * Initialization data for the @e2xgrader/labextension extension.
@@ -12,28 +12,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: '@e2xgrader/labextension:plugin',
   description: 'A JupyterLab extension which bundles e2xgrader extensions',
   autoStart: true,
-  optional: [ISettingRegistry],
-  activate: (
-    app: JupyterFrontEnd,
-    settingRegistry: ISettingRegistry | null
-  ) => {
+  requires: [ICellRegistry],
+  activate: (app: JupyterFrontEnd, cellRegistry: ICellRegistry) => {
     console.log('JupyterLab extension @e2xgrader/labextension is activated!');
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log(
-            '@e2xgrader/labextension settings loaded:',
-            settings.composite
-          );
-        })
-        .catch(reason => {
-          console.error(
-            'Failed to load settings for @e2xgrader/labextension.',
-            reason
-          );
-        });
-    }
+
+    console.log('Cell registry:', cellRegistry);
+    cellRegistry.cellRegistered.connect((_, { type, cellClass }) => {
+      console.log('Cell registered:', type, cellClass);
+    });
   }
 };
 
