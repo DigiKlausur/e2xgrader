e151a516caa757211c6e0259dce25cd8ba03b92a
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-12-19 17:28:45 +0100

    Add cell label to cell registry

--- packages/cell-core/src/cellregistry.ts
+++ packages/cell-core/src/cellregistry.ts
@@ -1,6 +1,6 @@
 import { Token } from '@lumino/coreutils';
 import { ISignal } from '@lumino/signaling';
-import { IE2xCell } from './model';
+import { IE2xCell } from './e2x_cell';
 
 export const ICellRegistry = new Token<ICellRegistry>(
   '@e2xgrader/cell-registry:ICellRegistry'
@@ -9,6 +9,7 @@ export const ICellRegistry = new Token<ICellRegistry>(
 export interface ICellRegistry {
   registerCellType(
     type: string,
+    label: string,
     cellClass: new (...args: any[]) => IE2xCell
   ): void;
   getCellClass(type: string): new (...args: any[]) => IE2xCell | undefined;
@@ -17,4 +18,5 @@ export interface ICellRegistry {
     { type: string; cellClass: new (...args: any[]) => IE2xCell }
   >;
   getCellTypes(): string[];
+  getCellLabel(type: string): string;
 }
