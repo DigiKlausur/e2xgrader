7e4b89c83149aab6ec2cbac957e97470b52c87ed
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-12-19 17:31:12 +0100

    Add GradingCellModel

--- packages/cell-core/src/model.ts
+++ packages/cell-core/src/model.ts
@@ -1,111 +1,159 @@
-import { MarkdownCell } from '@jupyterlab/cells';
-import { MimeModel } from '@jupyterlab/rendermime';
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
-import { RenderUtils } from './renderutils';
+import { ISharedCell, SharedCell } from '@jupyter/ydoc';
+import {
+  NbgraderMetadata,
+  NbgraderCellType,
+  NbgraderCellTypes
+} from './nbgrader';
+import { E2xGraderMetadata } from './e2x_cell';
 
-export const E2X_GRADER_SETTINGS_CLASS = 'e2x_grader_options';
-export const E2X_UNRENDER_BUTTON_CLASS = 'e2x_unrender';
-export const E2X_BUTTON_CLASS = 'e2x_btn';
-
-export const E2X_METADATA_KEY = 'extended_cell';
-
-export interface IE2xGraderMetadata {
-  type: string;
-  [key: string]: any;
-}
-
-export interface IE2xCell {
-  cell: MarkdownCell;
-  type: string;
-  settings: ISettingRegistry.ISettings;
-  options: object;
-  onCellRendered: () => void;
-  manipulateHTML: (html: Element) => void;
-  renderGraderSettings: (html: Element) => void;
-}
-
-export abstract class E2xCell implements IE2xCell {
-  cell: MarkdownCell;
-  type: string;
-  options: object;
-  settings: ISettingRegistry.ISettings;
-  editMode: boolean;
-
-  constructor(
-    cell: MarkdownCell,
-    type: string,
-    settings: ISettingRegistry.ISettings,
-    options: object = {}
-  ) {
-    this.cell = cell;
-    this.type = type;
-    this.settings = settings;
-    this.options = options;
-    // Make sure cells can not be unrendered
-    this.cell.showEditorForReadOnly = false;
-    this.editMode = true;
-
-    this.onSettingsChanged(settings);
-    settings.changed.connect(this.onSettingsChanged, this);
+export class GradingCellModel {
+  private readonly _cell: ISharedCell;
+  constructor(cell: ISharedCell) {
+    this._cell = cell;
   }
 
-  onSettingsChanged(settings: ISettingRegistry.ISettings): void {
-    const newEditMode = settings.get('edit_mode').composite as boolean;
-    if (newEditMode !== this.editMode) {
-      this.editMode = newEditMode;
-      if (this.cell.rendered) {
-        this.onCellRendered();
-      }
+  getMetadata(key: string): any {
+    return this._cell.getMetadata(key);
+  }
+
+  setMetadata(key: string, value: any): void {
+    this._cell.setMetadata(key, value);
+  }
+
+  setNestedMetadata(key: string, nestedKey: string, value: any): void {
+    const metadata = this.getMetadata(key) || {};
+    if (value === undefined) {
+      delete metadata[nestedKey];
+    } else {
+      metadata[nestedKey] = value;
     }
+    this.setMetadata(key, metadata);
   }
 
-  onCellRendered() {
-    this.cell.readOnly = true;
-    // Force a clean render of the cell.
-    // This is necessary because the cell will not be rendered again if the source is the same.
-    this.cell.renderer
-      .renderModel(
-        new MimeModel({
-          data: { 'text/markdown': this.cell.model.sharedModel.getSource() }
-        })
-      )
-      .then(() => {
-        this.render();
-      })
-      .catch(error => {
-        console.error('Error rendering cell', error);
-      });
+  removeNbgraderMetadata(): void {
+    this._cell.deleteMetadata(NbgraderMetadata.NBGRADER_METADATA_KEY);
   }
 
-  render(): void {
-    RenderUtils.getHTML(this.cell).then(html => {
-      this.manipulateHTML(html).then(() => {
-        if (this.editMode) {
-          this.renderGraderSettings(html);
-        }
-      });
-    });
+  removeE2xgraderMetadata(): void {
+    this._cell.deleteMetadata(E2xGraderMetadata.E2XGRADER_METADATA_KEY);
   }
 
-  abstract manipulateHTML(html: Element): Promise<void>;
+  setNbgraderMetadataKey(nestedKey: string, value: any): void {
+    this.setNestedMetadata(
+      NbgraderMetadata.NBGRADER_METADATA_KEY,
+      nestedKey,
+      value
+    );
+  }
 
-  renderGraderSettings(html: Element): void {
-    const container = document.createElement('div');
-    container.className = E2X_GRADER_SETTINGS_CLASS;
-    const unrenderButton = document.createElement('button');
-    unrenderButton.className =
-      E2X_UNRENDER_BUTTON_CLASS + ' ' + E2X_BUTTON_CLASS;
-    unrenderButton.textContent = 'Edit Cell';
-    unrenderButton.onclick = () => {
-      this.cell.readOnly = false;
-      this.cell.rendered = false;
-    };
-    container.appendChild(document.createElement('hr'));
-    container.appendChild(unrenderButton);
-    const optionContainer = document.createElement('div');
-    optionContainer.className = 'e2x-option-container';
-    container.appendChild(document.createElement('br'));
-    container.appendChild(optionContainer);
-    html.appendChild(container);
+  getE2xgraderMetadataKey(nestedKey: string, default_value: any = {}): any {
+    return this.e2xgraderMetadata?.[nestedKey] ?? default_value;
+  }
+
+  setE2xgraderMetadataKey(nestedKey: string, value: any): void {
+    this.setNestedMetadata(
+      E2xGraderMetadata.E2XGRADER_METADATA_KEY,
+      nestedKey,
+      value
+    );
+  }
+
+  get nbgraderMetadata(): NbgraderMetadata.INbgraderMetadata | undefined {
+    return this.getMetadata(NbgraderMetadata.NBGRADER_METADATA_KEY);
+  }
+
+  get e2xgraderMetadata(): E2xGraderMetadata.IE2xGraderMetadata | undefined {
+    return this.getMetadata(E2xGraderMetadata.E2XGRADER_METADATA_KEY);
+  }
+
+  get isNbgraderCell(): boolean {
+    return !!this.nbgraderMetadata;
+  }
+
+  get isE2xgraderCell(): boolean {
+    return !!this.e2xgraderMetadata;
+  }
+
+  get isGrade(): boolean {
+    return this.nbgraderMetadata?.grade ?? false;
+  }
+
+  set isGrade(value: boolean) {
+    this.setNbgraderMetadataKey('grade', value);
+  }
+
+  get isSolution(): boolean {
+    return this.nbgraderMetadata?.solution ?? false;
+  }
+
+  set isSolution(value: boolean) {
+    this.setNbgraderMetadataKey('solution', value);
+  }
+
+  get isTask(): boolean {
+    return this.nbgraderMetadata?.task ?? false;
+  }
+
+  set isTask(value: boolean) {
+    this.setNbgraderMetadataKey('task', value);
+  }
+
+  get isLocked(): boolean {
+    return this.nbgraderMetadata?.locked ?? false;
+  }
+
+  set isLocked(value: boolean) {
+    this.setNbgraderMetadataKey('locked', value);
+  }
+
+  get gradeId(): string | undefined {
+    return this.nbgraderMetadata?.grade_id;
+  }
+
+  set gradeId(value: string | undefined) {
+    this.setNbgraderMetadataKey('grade_id', value);
+  }
+
+  get points(): number | undefined {
+    return this.nbgraderMetadata?.points;
+  }
+
+  set points(value: number | undefined) {
+    this.setNbgraderMetadataKey('points', value);
+  }
+
+  get e2xgraderType(): string | undefined {
+    return this.e2xgraderMetadata?.type;
+  }
+
+  get gradingCellType(): string | undefined {
+    return (
+      this.e2xgraderType ??
+      NbgraderCellTypes.findMatchingCellType(this.nbgraderMetadata)
+    );
+  }
+
+  private matchesCellType(cellType: NbgraderCellType): boolean {
+    return NbgraderCellTypes.matchesCellType(this.nbgraderMetadata, cellType);
+  }
+
+  get isDescription(): boolean {
+    return this.matchesCellType(NbgraderCellType.DESCRIPTION);
+  }
+
+  get isAutograderTest(): boolean {
+    return this.matchesCellType(NbgraderCellType.AUTOGRADER_TEST);
+  }
+
+  get isAutograderSolution(): boolean {
+    return this.matchesCellType(NbgraderCellType.AUTOGRADED_ANSWER);
+  }
+
+  get isManualGradingCell(): boolean {
+    return this.matchesCellType(NbgraderCellType.MANUALLY_GRADED_ANSWER);
+  }
+
+  toJSON(): SharedCell.Cell {
+    return this._cell.toJSON();
   }
 }
